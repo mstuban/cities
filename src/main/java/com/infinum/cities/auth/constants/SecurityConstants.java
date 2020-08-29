552f2185cf9c23a07f10d62954e784be5630736f
Marko Štuban <marko.stuban@ag04.com>
2020-08-29 18:47:03 +0200

    Reformat code

--- src/main/java/com/infinum/cities/auth/constants/SecurityConstants.java
+++ src/main/java/com/infinum/cities/auth/constants/SecurityConstants.java
@@ -1,10 +1,5 @@
 package com.infinum.cities.auth.constants;
 
 public class SecurityConstants {
-    public static final String SECRET = "SECRET_KEY";
-    public static final long EXPIRATION_TIME = 900_000; // 15 mins
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/api/login";
     public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 }
--- src/main/java/com/infinum/cities/auth/jwt/JwtRequestFilter.java
+++ src/main/java/com/infinum/cities/auth/jwt/JwtRequestFilter.java
@@ -32,14 +32,15 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
         throws ServletException, IOException {
         final String requestTokenHeader = request.getHeader("Authorization");
-        String username = null;
+        String email = null;
         String jwtToken = null;
+
         // JWT Token is in the form "Bearer token". Remove Bearer word and get
         // only the Token
         if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
             jwtToken = requestTokenHeader.substring(7);
             try {
-                username = tokenUtil.getUsernameFromToken(jwtToken);
+                email = tokenUtil.getEmailFromToken(jwtToken);
             } catch (IllegalArgumentException e) {
                 System.out.println("Unable to get JWT Token");
             } catch (ExpiredJwtException e) {
@@ -48,19 +49,25 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         } else {
             logger.warn("JWT Token does not begin with Bearer String");
         }
-        // Once we get the token validate it.
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.userService.loadUserByUsername(username);
-            // if token is valid configure Spring Security to manually set
-            // authentication
+        // once we get the token validate it.
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userService.loadUserByUsername(email);
+
+            // if token is valid configure Spring Security to manually set authentication
             if (tokenUtil.validateToken(jwtToken, userDetails)) {
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                    userDetails, null, userDetails.getAuthorities());
-                usernamePasswordAuthenticationToken
-                    .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                // After setting the Authentication in the context, we specify
-                // that the current user is authenticated. So it passes the
-                // Spring Security Configurations successfully.
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                    new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                    );
+                usernamePasswordAuthenticationToken.setDetails(
+                    new WebAuthenticationDetailsSource()
+                        .buildDetails(
+                            request
+                        ));
+                // after setting the Authentication in the context, we specify
+                // that the current user is authenticated.
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
