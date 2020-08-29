de311d51676536ad6bdbec79f7e48fd6a6f24f30
Marko Štuban <marko.stuban@ag04.com>
2020-08-29 23:40:35 +0200

    Reformat code

--- src/test/java/com/infinum/cities/stub/AuthenticationRequestStubFactory.java
+++ src/test/java/com/infinum/cities/stub/AuthenticationRequestStubFactory.java
@@ -3,6 +3,7 @@ package com.infinum.cities.stub;
 import com.infinum.cities.model.auth.AuthenticationRequest;
 
 public class AuthenticationRequestStubFactory {
+
     public static AuthenticationRequest authRequest = new AuthenticationRequest(
         "johnny.cash@gmail.com",
         "pass"
